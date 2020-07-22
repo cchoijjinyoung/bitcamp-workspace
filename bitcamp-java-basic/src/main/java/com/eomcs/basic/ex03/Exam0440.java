31ff9597501d952e2ba16ee8873c8db9ea45a8a7
cchoijinyoung <cgy7127@github.com>
2020-07-22 20:12:52 +0900

    Day8

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0440.java
@@ -0,0 +1,28 @@
+
+
+
+package com.eomcs.basic.ex03;
+
+
+
+public class Exam0440 {
+    public static void main(String[] args) {
+      
+      System.out.println(true);
+      System.out.println(false);
+      
+      System.out.println(4 < 5);
+      System.out.println(4 > 5);
+      
+      System.out.println(true && false);
+      System.out.println(true || false);
+      
+      System.out.println(65 + 1 == 'B');
+      System.out.println(65 == 'B');
+      
+      
+      
+    
+}
+}
+
--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0500.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0500.java
@@ -1,15 +1,17 @@
-// Exam0400.java
+
 // 주제 : 실행할 수 있는 클래스
 
 package com.eomcs.basic.ex03;
 
 
 //# 문자 리터
-public class Exam0430 {
+public class Exam0500 {
     public static void main(String[] args) {
       
       System.out.println(0x0041);
       System.out.println(0x41);
+      System.out.println((char)'각');
+      System.out.println((char)('각' + 1));
       System.out.println((char)0x41);
       System.out.println('A');
       System.out.println((int)'A');
@@ -20,7 +22,7 @@ public class Exam0430 {
         System.out.println((char)i);
         
         //숫자를 true/false 로 변환할 수 없다.
-        System.out.println((boolean)1);
+        //System.out.println((boolean)1);
       }
       
     
